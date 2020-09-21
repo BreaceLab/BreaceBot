97fa9cf3c89388cf4c753c1f888824d2e8b83709
Luiscloud <luisfelipe.assuncao@hotmail.com>
2020-09-21 00:17:11 -0400

    Level agora mostra a posição

--- src/commands/LevelCommand.js
+++ src/commands/LevelCommand.js
@@ -18,9 +18,14 @@ module.exports = class LevelCommand extends Command {
 
     const features = Object.entries(context.levelFeatures)
 
+    const rank = await this.client.database.models.users.find().sort({ level: -1 }).sort({ xp: -1 })
+
+    const position = rank.indexOf(rank.find(({ _id }) => _id === data._id))
+
     embed.setDescription([
       `• **Usuário:** \`${target.tag}\``,
-      `• **Level/Xp:** \`${data.level} (${data.xp}/${data.level ** 5 + (100 * (data.level * 2))})\``, '',
+      `• **Level:** \`${data.level} (${data.xp}/${data.level ** 5 + (100 * (data.level * 2))} Xp)\``,
+      `• **Rank:** \`#${position + 1}\``, '',
       `• **Interaja para liberar os benefícios:**\n${features.filter(([level]) => data.level < level).map(([level, { reward }]) => `-  *\`[${level}]. ${reward}\`*`).join('\n')}`
     ])
     embed.setThumbnail(target.displayAvatarURL({ dynamic: true }))
