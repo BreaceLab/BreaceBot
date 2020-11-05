d5ff4081d370dff819ec33e4f0d2896163b53837
LuckShiba <luckshiba@protonmail.com>
2020-11-04 21:08:24 -0300

    Caching owners on ready event

--- src/listeners/ReadyListener.js
+++ src/listeners/ReadyListener.js
@@ -10,6 +10,7 @@ module.exports = class ReadyListener extends Listener {
 
   run () {
     this.user.setActivity(this.config.prefixes.map(prefix => `${prefix}help`).join(' | '), { type: 'PLAYING' })
+    this.config.owners.forEach(user => this.users.fetch(user))
     console.log(this.user.username, 'is ready to use.')
   }
 }
